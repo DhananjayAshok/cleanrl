9cfd52bd5c736716c1b77ec32a280a96307ad57b
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-02-11 14:54:08 -0800

    idk bro

--- cleanrl/ddpg_continuous_action.py
+++ cleanrl/ddpg_continuous_action.py
@@ -159,7 +159,8 @@ if __name__ == "__main__":
 
     # env setup
     envs = gym.vector.SyncVectorEnv(
-        [make_env(args.env_id, args.seed, 0, args.capture_video, run_name)]
+        [make_env(args.env_id, args.seed, 0, args.capture_video, run_name)],
+        autoreset_mode=gym.vector.AutoresetMode.SAME_STEP,
     )
     assert isinstance(
         envs.single_action_space, gym.spaces.Box
