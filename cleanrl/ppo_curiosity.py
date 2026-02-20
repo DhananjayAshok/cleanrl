71300333ed360d766de00b6635a4e88ecad6b4a0
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-02-20 13:38:52 -0800

    Automatic commit: Added all

--- cleanrl/ppo_curiosity.py
+++ cleanrl/ppo_curiosity.py
@@ -168,7 +168,7 @@ if __name__ == "__main__":
     args = tyro.cli(Args)
     args.batch_size = int(args.num_envs * args.num_steps)
     args.minibatch_size = int(args.batch_size // args.num_minibatches)
-    args.num_iterations = args.total_timesteps // args.batch_size
+    args.num_iterations = max(args.total_timesteps // args.batch_size, 1)
     run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
     if args.track:
         import wandb
